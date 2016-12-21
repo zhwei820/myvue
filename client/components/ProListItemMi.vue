feaa4eadeea0c9f01459b4bae44aad39878f191e
zhouwei <zhouwei@dianjoy.com>
2016-12-21 18:14:13 +0800

    +

--- client/components/ProListItemMi.vue
+++ client/components/ProListItemMi.vue
@@ -1,30 +1,30 @@
 <template>
-    <div>
-        <a class="item" href="/#/product/list?id=354" data-stat-id="b3883d4c6dc02658" onclick="_msq.push(['trackEvent', '221a86c5a4688b35-b3883d4c6dc02658', '/#/product/list?id=354', 'pcpid']);">
+
+    <div class="J_linksign-customize">
+        <a class="item" href="/#/product/view?product_id=4984" data-stat-id="69b508818cf387e2" onclick="_msq.push(['trackEvent', '221a86c5a4688b35-69b508818cf387e2', '/#/product/view?product_id=4984', 'pcpid']);">
             <div class="img">
-                <img class="ico lazy" data-src="//i8.mifile.cn/v1/a1/73d581f3-3d92-e7f5-a44a-4ed3203cd16f.webp" src="//i8.mifile.cn/v1/a1/73d581f3-3d92-e7f5-a44a-4ed3203cd16f.webp"
+                <img class="ico lazy" data-src="//i8.mifile.cn/v1/a1/df70403b-6e69-0822-e92a-be464e868698.webp" src="//i8.mifile.cn/v1/a1/df70403b-6e69-0822-e92a-be464e868698.webp"
                     style="transform-origin: 0px 0px 0px; opacity: 1; transform: scale(1, 1);">
-                <!--vue-if-->
+                <img class="tag lazy" src="//c1.mifile.cn/f/i/f/mishop/iic/xp.png">
             </div>
             <div class="info">
                 <div class="name">
                     <p>
-                        红米手机3S
+                        小米米家电动滑板车
                     </p>
                 </div>
                 <div class="brief">
                     <p>
-                        指纹识别 / 全网通双卡双待 / 金属机身 / 4100mAh大电池
+                        极简几何设计，1分钟轻松上手 / 30公里超长续航 / 双重刹车系统 / 便携折叠
                     </p>
                 </div>
                 <div class="price">
                     <p>
-                        699元 起
+                        1999元
                     </p>
                 </div>
             </div>
         </a>
-        <!--v-repeat-body.items-->
     </div>
 
 </template>
@@ -68,7 +68,6 @@ export default {
         width: 250px;
         margin-right: 5px;
         display: inline-block;
-        
     }
     
     .name {
@@ -87,5 +86,15 @@ export default {
     .price {
         font-size: 16px;
         color: #FE4900;
+    }
+    
+    .img .tag {
+        position: absolute;
+        width: 69px;
+        height: 69px;
+        top: 0;
+        left: 5px;
+        opacity: .9!important;
+        z-index: 2;
     }
 </style>
